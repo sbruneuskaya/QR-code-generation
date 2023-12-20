61c6a379dda82ead8d5059643698447026686bf8
Bruneuskaya <bs@primexbt.ch>
2023-12-20 17:21:01 +0300

    main: added styles

--- src/App.js
+++ src/App.js
@@ -8,35 +8,37 @@ function App() {
 
     const [inputValue, setInputValue]=useState("")
   return (
-    <div className="container">
-      <h1>your link to generate a QR code</h1>
-      <div className={"link"}>
-          <Box
-              sx={{
-                  width: '100%',
-              }}
-          >
-              <div>
-                  <TextField
-                      id="fullWidth"
-                      label="your link"
-                      fullWidth
-                      maxRows={4}
-                      onChange={(e)=>setInputValue(e.target.value)}
-                  />
-              </div>
-          </Box>
-      </div>
+   <div className={"wrapper"}>
+       <div className="container">
+           <h1>your link to generate a QR code</h1>
+           <div className={"link"}>
+               <Box
+                   sx={{
+                       width: '100%',
+                   }}
+               >
+                   <div>
+                       <TextField
+                           id="fullWidth"
+                           label="your link"
+                           fullWidth
+                           maxRows={4}
+                           onChange={(e)=>setInputValue(e.target.value)}
+                       />
+                   </div>
+               </Box>
+           </div>
 
-       <div className="wrapperCode">
-         <QRCode
-             size={256}
-             style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-             value={inputValue}
-             viewBox={`0 0 256 256`}
-         />
+           <div className="wrapperCode">
+               <QRCode
+                   size={256}
+                   style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                   value={inputValue}
+                   viewBox={`0 0 256 256`}
+               />
+           </div>
        </div>
-    </div>
+   </div>
   );
 }
 
